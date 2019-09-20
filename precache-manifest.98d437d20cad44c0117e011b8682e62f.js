b18c1a4062aa0b8aaa929d5b12f5344ab339fbb8
XxJohnWickxX <tumjoncrm@gmail.com>
2019-09-20 10:49:47 -0500

    Updates

---
+++ precache-manifest.98d437d20cad44c0117e011b8682e62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e402081becc588e7eea5046e808715c",
+    "url": "/curri_online/index.html"
+  },
+  {
+    "revision": "c3b152e6f50bccc8b0de",
+    "url": "/curri_online/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3626c35a5637acbf34e0",
+    "url": "/curri_online/static/js/2.4020f3dc.chunk.js"
+  },
+  {
+    "revision": "c3b152e6f50bccc8b0de",
+    "url": "/curri_online/static/js/main.1d0c520d.chunk.js"
+  },
+  {
+    "revision": "88dfe9a98626ece99ed5",
+    "url": "/curri_online/static/js/runtime-main.d8da1368.js"
+  },
+  {
+    "revision": "8345ba3b9c6bfb8fbad9f49b414acd53",
+    "url": "/curri_online/static/media/icon_software.8345ba3b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/curri_online/precache-manifest.a69e068007564c820ce6164905fccc29.js"
+  "/curri_online/precache-manifest.98d437d20cad44c0117e011b8682e62f.js"
 );
 
 self.addEventListener('message', (event) => {
