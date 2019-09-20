51facb77948cdae4812cd9dbcf8b1fc4468fb4d0
XxJohnWickxX <tumjoncrm@gmail.com>
2019-09-20 10:36:22 -0500

    Updates

---
+++ precache-manifest.a69e068007564c820ce6164905fccc29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa9368afd71d8fee5179ec460e10d51",
+    "url": "/curri_online/index.html"
+  },
+  {
+    "revision": "b40da72b91c7bb398e5f",
+    "url": "/curri_online/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b482d18861b5bf26ad7a",
+    "url": "/curri_online/static/js/2.d46452a8.chunk.js"
+  },
+  {
+    "revision": "b40da72b91c7bb398e5f",
+    "url": "/curri_online/static/js/main.a8d075cc.chunk.js"
+  },
+  {
+    "revision": "88dfe9a98626ece99ed5",
+    "url": "/curri_online/static/js/runtime-main.d8da1368.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/curri_online/static/media/logo.25bf045c.svg"
+  }
+]);
