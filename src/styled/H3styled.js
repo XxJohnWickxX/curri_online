09faf62703103eb628f96406734db9a3663749d5
XxJohnWickxX <tumjoncrm@gmail.com>
2019-09-20 11:33:53 -0500

    produccion

--- src/styled/H3styled.js
+++ src/styled/H3styled.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledH2 = styled.h2`
-    font-family: 'Roboto', sans-serif;
-    font-weight: 300;
-    margin: 1em 0 0 0;
-    color: #FF9445;
+const StyledH3 = styled.h2`
+    font-weight: 400;;
+    margin: .5em 0;
+    color: #212121;
 `;
 
-const H2styled = ({ children }) => <StyledH2>{ children }</StyledH2>;
+const H3styled = ({ children }) => <StyledH3>{children}</StyledH3>;
 
-export default H2styled;
+export default H3styled;
